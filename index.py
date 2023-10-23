143e8ac39e4d28381db4d68e8b5f6ed49a7605e1
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2023-10-23 16:31:36 -0700

    added deposit, withdraw,balance, and interest

---
+++ index.py
@@ -0,0 +1,30 @@
+class BankAccount:
+    def __init__(self, full_name, account_number, balence) :
+        self.name:full_name
+        self.number: self.account_number    
+        self.balence: 0.0
+
+    # def account_number(self):
+    #     return str(random.randit(10000000, 99999999))
+    
+    def deposit(self, amount):
+        self.balence += amount
+        print(f"Amount deposited: {amount:.2f} new balence: {self.balence: .2f}")
+
+    def withdraw(self, amount):
+        if amount is <= self.balence:
+            self.balence = amount
+            print(f"Amount withdrawn: {amount:.2f} New balence: {self.balence: .2f}")
+
+        else:
+            print("Insufficient funds")
+            self.balence = 10
+
+    def get_balence(self):
+        print(f"Account Balance for {self.full_name} (Account number: {self.account_number}: {self.balence: .2f}")
+        return self.balence
+   
+    def add_interest(self):
+        monthly_interest = self.balence * 0.00083
+        self.balence += monthly_interest
+        
